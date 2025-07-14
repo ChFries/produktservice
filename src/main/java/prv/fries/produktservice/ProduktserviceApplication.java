5f0f7d00dc05824c0dc764ad27cb0591bf1ea3c8
chris <ch.fries@gmx.net>
2025-07-14 11:58:31 +0200

    Initial commit

---
+++ src/main/java/prv/fries/produktservice/ProduktserviceApplication.java
@@ -0,0 +1,13 @@
+package prv.fries.produktservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduktserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProduktserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/prv/fries/produktservice/ProduktserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package prv.fries.produktservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduktserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
