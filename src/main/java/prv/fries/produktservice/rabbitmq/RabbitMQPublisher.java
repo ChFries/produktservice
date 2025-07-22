b5eded43e989041b549cf343b01ceb8dbc317436
chris <ch.fries@gmx.net>
2025-07-22 18:57:02 +0200

    feat(#19): Update logging

--- src/main/java/prv/fries/produktservice/rabbitmq/RabbitMQPublisher.java
+++ src/main/java/prv/fries/produktservice/rabbitmq/RabbitMQPublisher.java
@@ -1,12 +1,14 @@
 package prv.fries.produktservice.rabbitmq;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import prv.fries.produktservice.generated.ProduktVerfuegbarDto;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RabbitMQPublisher {
 
     private static final String ROUTING_KEY = "bestellung.pruefung.abgeschlossen";
@@ -14,6 +16,7 @@ public class RabbitMQPublisher {
     private final RabbitTemplate rabbitTemplate;
 
     public void publishPruefungAbgeschlossen(ProduktVerfuegbarDto verfuegbareProdukte) {
+        log.info("Pruefung abgeschlossen für {}", verfuegbareProdukte.getBestellId());
         rabbitTemplate.convertAndSend(ROUTING_KEY, verfuegbareProdukte);
     }
 }
